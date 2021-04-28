f243ac24ff3f152e6bcfc525dc091f4df3876782
rodrigo <rodri.oliveira1110@gmail.com>
2021-04-28 15:25:36 -0300

    H2 Database, Test Profile, JPA

--- src/main/java/com/rodrigooliveira/aulacrud/entities/User.java
+++ src/main/java/com/rodrigooliveira/aulacrud/entities/User.java
@@ -2,11 +2,17 @@ package com.rodrigooliveira.aulacrud.entities;
 
 import java.io.Serializable;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
